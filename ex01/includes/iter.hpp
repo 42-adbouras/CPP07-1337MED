fc852a44d8c5ca01d2cfb4d66dd376a0ea71469f
42-adbouras <wikaz077@gmail.com>
2025-05-21 20:29:08 +0100

    start ex01

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -1,35 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adbouras <adbouras@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/21 12:16:49 by adbouras          #+#    #+#             */
-/*   Updated: 2025/05/21 19:54:06 by adbouras         ###   ########.fr       */
+/*   Created: 2025/05/21 13:16:07 by adbouras          #+#    #+#             */
+/*   Updated: 2025/05/21 13:17:38 by adbouras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template<typename Type>
-void	swap( Type& left, Type& right )
-{
-	Type	temp;
+#include <iostream>
+#include <string>
 
-	temp  = left;
-	left  = right;
-	right = temp;
-}
-
-template<typename Type>
-Type&	min( Type& left, Type& right )
-{
-	return (left < right) ? left : right;
-}
-
-template<typename Type>
-Type&	max( Type& left, Type& right )
-{
-	return (left > right) ? left : right;
-}
